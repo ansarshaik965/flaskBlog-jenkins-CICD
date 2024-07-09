708d5f45dd44150f2d5b888fd1e55424510101ff
ansarshaik965 <ansarshaik965@gmail.com>
2024-07-09 23:00:15 +0530

    flaskblog added

---
+++ jenkins_install.sh
@@ -0,0 +1,12 @@
+# Open jdk 17 installation 
+sudo apt install openjdk-17-jdk openjdk-17-jre -y
+
+# jenkins installation 
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
